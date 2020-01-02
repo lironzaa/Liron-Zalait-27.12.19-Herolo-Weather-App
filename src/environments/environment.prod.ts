ee70dcccd270943a9feafe9f2a2c1ee35d2e70a5
lironzaa <lironzaa@gmail>
2020-01-03 00:39:52 +0200

    before deployment fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherAPIKey: 'Y8RgWkfnkR5YW0qdYKAo37G47EibuAGA',
+  apiDaily: 'http://dataservice.accuweather.com/currentconditions/v1',
+  apiForecast: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day',
+  apiAutocomplete: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+  apiGeoLocation: 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search',
 };
